7ccfec58f0dbc2a7aee3a0beca0bf2ef7dd5630c
arwansyah <arwansyah2787@gmail.com>
2024-11-03 14:01:24 +0700

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('hamburger-button').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+  });
+
+
+  
+
+   
